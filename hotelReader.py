26e61dfb040071cdf5d404c96b06b90f382cad93
Wade Fletcher <wbfletch@gmail.com>
2020-02-16 20:50:29 -0500

    started making HotelReader.py

---
+++ hotelReader.py
@@ -0,0 +1,17 @@
+"""Python hotel data reader created for CS Club @ IU, Brady Anderson 2020"""
+# module imports
+
+# import fuzzywuzzy thing
+
+# global configs
+"""
++================================+
+  hotel data reader
+  Copyright CS Club @ IU, 2020
++================================+
+"""
+
+def hotelData(location, price, priceKeyword, petFriendly):
+    # fuzzy wuzzy matching of string values
+
+    # price handeling
