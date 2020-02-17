12773b9a5226b17639cf043cc5b0efca6ab595fa
Brady <brjoande@iu.edu>
2020-02-16 20:26:18 -0500

    updated dataset

--- HotelWebScraper/getHotels.py
+++ HotelWebScraper/getHotels.py
@@ -37,7 +37,7 @@ with open('hotelData2.csv', 'w') as file:
         location = soup1.find_all("div", {"class":"hotel-card-dreamstyles__DistanceDiv-sc-1xoiocm-7 hpvmLI"})
         price = soup1.find_all("p", {"class": "hotel-card-dreamstyles__LeadRate-sc-1xoiocm-6 crEdua"})
 
-        writer.writerow(["Name", "Location", "Price per Night"])
+        writer.writerow(["Name", "Location", "Price per Night", "Is Pet Friendly"])
         for i in range(len(name)):
             a = name[i].text.encode("utf8")
             b = location[i].text.encode("utf8")
