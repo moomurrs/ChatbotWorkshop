3e39a2c08edfa14f8e5012d3ae3f3243fd656d3d
Brady <brjoande@iu.edu>
2020-02-17 23:55:30 -0500

    added hotel data matching

--- HotelData.py
+++ HotelData.py
@@ -18,19 +18,41 @@ from fuzzywuzzy import fuzz, process
 df = pd.read_csv("hotels.csv")
 CITY_NAMES = [x.lower() for x in list(df["City"])]
 STATE_NAMES = [x.lower() for x in list(df["State"])]
-
+PRICE = [x for x in list(df["Price"])]
 
 # all are set to None as default because we will likely have incomplete data
 # you'll need to check "if state:", "if price:" etc. before using each one
 def findHotels(city=None, state=None, price=None, petFriendly=None, price_variance=30):
-    # fuzzy wuzzy matching of string values
-    print(fuzz.token_sort_ratio("cowboys", "Dallas Cowboys"))
-
-    # price handeling price and priceKeyword based
-
-    # Price keyword -> around
-
-
+    # clean parameters to be accepted w/ fuzzywuzzy
+    s = process.extract(state, STATE_NAMES, limit=1)
+    p = []
+    h = []
+    pf = []
+    c = []
+    # iterate through all dataframes to get relevant data
+    for i, j in df.iterrows(): 
+      # get hotels in state and perform price matching - keep track of hotels and pet friendly
+      if(j[2] == s[0][0].upper()):
+        p.append(str(j[3]))
+        h.append(j[0])
+        pf.append(j[4])
+        c.append(j[1])
+    # check other preferences of relevant data
+    max = price + price_variance
+    min = price - price_variance
+    bestOptions = []
+    priceOptions = []
+    petOptions = []
+    # process relevant data
+    for i in range(len(h)):
+      if(p[i] < max and p[i] > min and pf[i] == petFriendly):      # both conditions satisfied!
+        bestOptions.append({"hotel": h[i], "city":c[i], "price": p[i], "pets-allowed":pf[i]})
+      elif(int(p[i]) > min and int(p[i]) < max):                # only good price
+        priceOptions.append({"hotel": h[i], "city":c[i], "price": p[i], "pets-allowed":pf[i]})
+      elif(pf[i] == petFriendly):      # only petfriendly
+        petOptions.append({"hotel": h[i], "city":c[i], "price": p[i], "pets-allowed":pf[i]})
+    return bestOptions, priceOptions, petOptions
+        
 if __name__ in "__main__":
-    findHotels("a", "a", "", 0)
+    findHotels("AZ", "Ri", 100, 0)
 
